3984c37c5c68c8005cd0f7eb9deaf556737de965
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-06-14 21:18:45 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let sideMenu = document.querySelector('aside');
+let menuBtn = document.querySelector('#menu-btn');
+let closeBtn = document.querySelector('#close-btn');
+let themeToggler = document.querySelector('.theme-toggler');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
+})
